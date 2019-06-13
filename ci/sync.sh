2bce8c47dc68b5c835ba399e89a27f6a32bc5e7b
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2019-06-13 21:54:24 +0200

    clear ROS_VERSION_EOL, use final snapshot for EOL distros

--- ci/sync.sh
+++ ci/sync.sh
@@ -10,36 +10,29 @@ DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HEADER="# DO NOT EDIT!\n# This file  was auto-generated with ./sync.sh at $(LC_ALL=C date)\n"
 INJECT_MAINTAINER="/^FROM/a LABEL MAINTAINER \"$(git config --get user.name)\" <$(git config --get user.email)>"
 
-function export_dockerfile {
+function export_dockerfile ()(
     echo $*
     ROS_DISTRO=$1
     ROS_REPO=$2
-    OS_NAME=
-    OS_CODE_NAME=
-    DOCKER_BASE_IMAGE=
-    ROS_REPOSITORY_PATH=
+    OS_CODE_NAME=$3
+    DOCKER_BASE_IMAGE=$4
 
-    source $DIR_ICI/src/env.sh
-    OS_CODE_NAME=${3:-$OS_CODE_NAME}
-    DOCKER_BASE_IMAGE=${4:-$DOCKER_BASE_IMAGE}
-
-    local path=$DIR_THIS/$ROS_DISTRO-${5:-$OS_CODE_NAME}${ROS_REPO#ros}
+    ROS_REPO=${EOL_REPO:-$2} source $DIR_ICI/src/env.sh
+    local path=$DIR_THIS/${ROS_DISTRO}-${5:-$OS_CODE_NAME}${2#ros}
     mkdir -p "$path"
 
     echo -e "$HEADER" > $path/Dockerfile
     ici_generate_default_dockerfile | sed "$INJECT_MAINTAINER" >> $path/Dockerfile
-}
+)
 
 for r in ros ros-shadow-fixed; do
     for d in hydro indigo jade kinetic lunar melodic; do
         export_dockerfile $d $r
     done
-    export_dockerfile kinetic $r jessie debian:jessie
-    export_dockerfile lunar $r yakkety
-    export_dockerfile lunar $r zesty
-    export_dockerfile lunar $r stretch debian:stretch
+    EOL_REPO=final export_dockerfile kinetic $r jessie debian:jessie
+    EOL_REPO=final export_dockerfile lunar $r yakkety
+    EOL_REPO=final export_dockerfile lunar $r zesty
     export_dockerfile lunar $r stretch debian:stretch
     export_dockerfile melodic $r stretch debian:stretch
-    export_dockerfile melodic $r artful
+    EOL_REPO=final export_dockerfile melodic $r artful
 done
-
