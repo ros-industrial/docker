f48dff930c185172cdcd2e9826ffa1de8645e0cf
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2019-06-13 22:52:01 +0200

    patch old-releases

--- ci/sync.sh
+++ ci/sync.sh
@@ -36,3 +36,7 @@ for r in ros ros-shadow-fixed; do
     export_dockerfile melodic $r stretch debian:stretch
     EOL_REPO=final export_dockerfile melodic $r artful
 done
+
+for path in $DIR_THIS/*{yakkety,zesty,artful}*; do
+   sed -i "/^LABEL MAINTAINER/a RUN sed -i -re 's/([a-z]{2}\.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list" $path/Dockerfile
+done
